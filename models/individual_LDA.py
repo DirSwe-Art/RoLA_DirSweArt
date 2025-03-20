b34b194d605434438b6eaf4e7acfe2d45f5c11c4
DirarSweidan <dirar.sweidan.research@gmail.com>
2025-03-20 15:33:04 +0100

    updates

--- models/individual_LDA.py
+++ models/individual_LDA.py
@@ -1,33 +1,43 @@
-### REPAD2 Algorithm ###
+### RoLA Algorithm ###
 
-"""
-THE PLAN
-
-Making the program start from any predefined time, and then continue to fetch data from that time and onwards.
-And so it can process older data and then catch up to the present time.
-
-It therefore processes a batch of 3 from the earliest timestamp in the range, and then updates the start time for the next iteration 
-by incrementing the start time by a set time to both avoid duplicate events and to eventually catch up to the present time.
-
-The program will run indefinitely, and will continue to fetch data from the InfluxDB and process it in batches of 3 until the program is stopped.
-I there are not enough events for a batch, the program will wait for a set time before trying again.
-When a batch of three is available it will be processed and the it will again wait for another event to be available.
-
-This way it is both flexible and efficient, and can be easily used to process either data in real-time or historical data
-
-
-For each batch it follows the algorithm of RePAD2
-
-
-To-Do:
-
-Store actual values (Store whole event?) with predicted values in sliding window 
-
-"""
-def individual_LDA(T, batch_events, next_event, M, flag, actual_value, predicted_value, sliding_window_AARE):
+def is_anomaly(T, vatiable_name, state):
+	"""
+    This function is an LDA-based anomaly detection function that checks if a given
+	data point (variable Vx at time T) is an anomaly. 
+	It updates variable LDA's parameters dynamically.
+	
+	Parameters:
+	===========
+	T:					The given time point of the data point.
+	variable_name:		The name of the variable of the data point. 
+	state:				A nested dictionary containes dictionaries associated with each variable, which containes
+						specific arguments for an LDA to store and update relevant data, such as:
+	
+	  *	batch_events:	A batch (type deque) of four time points events D_T-3, D_T-2, D_T-1, and D_T. It should be updated in each iteration.
+						It is used for predicting D_T+1 using batch_events[1:], and predicting D_T using batch_events[0:-1].	
+	  *	next_event:		The event to predict next when T = 0, 1, 2, 3, 4, 5, and 6. It should be updated in each iteration.
+	  *	M:				A trained LSTM model. Default value is "None". 
+	  *	flag:			A flag that indicates whether an anomaly was ditected (falg=False) in the previous iteration. Default value is "True".
+	  *	actual_value:	A deque type window of three elements to store the actual value of events within three iterations to calculate the AARE.
+	  *	predicted_value: A deque type window of three elements to store the predicted value of events within three iterations to calculate the AARE.
+	  *	sliding_window_AARE: A deque type window to store the AARE resulted in each iteration in order to calculate the threhold later.
+	
+	Return:				The flag indicating the anomaly, together with updated batch events, next events, actual_value, predicted_value, 
+	=======				sliding_window_AARE, and the model that will be used in the next iteration.
+    """
+	
 	# For printing the values
 	AARE_T = 0
 	Thd = 0
+	variable_state   = state[variable_name]
+	
+	batch_events	= variable_state["batch_events"]
+	next_event    	= variable_state["next_event"]
+	M                   	= variable_state["M"]
+	flag			= variable_state["flag"]
+	actual_value	= variable_state["actual_value"]
+	predicted_value	= variable_state["predicted_value"]
+	sliding_window_AARE = variable_state["sliding_window_AARE"]
 
 	# Initialize the LDA
 	if T >= 2 and T < 5:
@@ -50,7 +60,8 @@ def individual_LDA(T, batch_events, next_event, M, flag, actual_value, predicted
 		# Write the results to InfluxDB.
 		#write_result(batch_events[-1].get_time(), T, batch_events[-1].get_value(), predicted_value[-2], AARE_T, Thd, write_api) 
 		
-		return batch_events, next_event, M, actual_value, predicted_value, sliding_window_AARE, flag
+		#return batch_events, next_event, M, actual_value, predicted_value, sliding_window_AARE, flag
+		return False
 
 	elif T >= 5 and T < 7:
 		# Calculate AARE and append to sliding window.
@@ -68,7 +79,8 @@ def individual_LDA(T, batch_events, next_event, M, flag, actual_value, predicted
 		# Write the results to InfluxDB.
 		#write_result(batch_events[-1].get_time(), T, batch_events[-1].get_value(), predicted_value[-2], AARE_T, Thd, write_api) 
 
-		return batch_events, next_event,  M, actual_value, predicted_value, sliding_window_AARE, flag
+		#return batch_events, next_event,  M, actual_value, predicted_value, sliding_window_AARE, flag
+		return False
 		
 	# Make predictions of D_T by training M with D_T-3, D_T-2, D_T-1, i.e., (batch_events)[0:-1]
 	elif T >= 7 and flag == True:							
@@ -118,8 +130,8 @@ def individual_LDA(T, batch_events, next_event, M, flag, actual_value, predicted
 				
 		# Write the results to InfluxDB																							### delete later
 		#write_result(batch_events[-1].get_time(), T, batch_events[-1].get_value(), predicted_value[-1], AARE_T, Thd, write_api) 	### delete later
-		return batch_events, next_event, M, actual_value, predicted_value, sliding_window_AARE, flag
-			
+		#return batch_events, next_event, M, actual_value, predicted_value, sliding_window_AARE, flag
+		return not flag	
 
 	elif T >= 7 and flag == False:
 		# Train an LSTM model with D_T-3, D_T-2, D_T-1
@@ -149,8 +161,8 @@ def individual_LDA(T, batch_events, next_event, M, flag, actual_value, predicted
 		
 		# Write the results to InfluxDB																							### delete later
 		#write_result(batch_events[-1].get_time(), T, batch_events[-1].get_value(), predicted_value[-1], AARE_T, Thd, write_api) 	### delete later
-		return batch_events, next_event, M, actual_value, predicted_value, sliding_window_AARE, flag
-		
+		#return batch_events, next_event, M, actual_value, predicted_value, sliding_window_AARE, flag
+		return not flag
 		
 # Setting up the InfluxDB to consume data
 influxdb_url = "http://localhost:8086"
@@ -166,76 +178,78 @@ client = InfluxDBClient(url=influxdb_url, token=token, org=org, username=usernam
 write_api = client.write_api(write_options=ASYNCHRONOUS)
 query_api = client.query_api()
 
-# RoLA specific
-T = 0
+# Sttings
+T = 0											# Time point
+A, L_var, L_data       = [], [], []					# Lists for storing polling results
+C_agree, C_disagree = 0, 0						# Counters
+poll_interval = 1  								# Time parameter: Second(s)
+time_increment = 1 								# Time parameter: Second(s)
+start_time = "2021-10-28T00:00:00Z" 				# The timestamp of the first event to start with in the given time series dataset
 
-# Data structures for training and predicting
-batch_events = deque(maxlen=4)					# Events from time points: T-3, T-2, T-1, and T
-next_event = deque(maxlen=1)						# Used in the case of predicting D_T+1
 
-# Sliding window for calculating the Threshold
-actual_value = deque([0] * 3, maxlen=3)			# To append the actual event value in current iteration 
-predicted_value = deque([0] * 3, maxlen=3)		# To append the predicted event value in current iteration 
-sliding_window_AARE = deque(maxlen=8064)			# Tp append the resulting AARE in the current iteration
+# Initialize a state dictionary for each variable where each LDA stores and updates its arguments, such as 
+# the batch events, trained model, the flag, and others.
+variables = ["SEB45Salinity", "SEB45Conductivity", "OptodeConcentration", "OptodeSaturation", 
+			"C3Temperature", "FlowTemperature", "OptodeTemperature", "C3Turbidity", "FlowFlow"]
+state        = { 
+			key:{"batch_events": 	deque(maxlen=4), 
+				 "next_event": 		deque(maxlen=1), 
+				 "actual_value": 	deque([0] * 3, maxlen=3), 
+				 "predicted_value": 	deque([0] * 3, maxlen=3), 
+				 "sliding_window_AARE": deque(maxlen=8064), 
+				 "M": 				None, 
+				 "flag": 			True }
+			for key in variables 
+			}
 
-M = 0											# Trained LSTM model 
-flag = True										# True: no anomaly was ditected in the previous iteration
 
-# Time parameters
-poll_interval = 1  								# Second(s)
-time_increment = 1 								# Second(s)
-start_time = "2021-10-28T00:00:00Z" 				# The time of the first event in the time series data
 	
 while True:
-	
-	# Construct the Flux query
-	#query = f'''
-	#from(bucket: "{bucket}")
-	#	|> range(start: time(v: "{start_time}"))
-	#	|> filter(fn: (r) => r["_measurement"] == "{measurement}")
-	#'''
-	# Query the data
-	#events = list(query_api.query_stream(org=org, query=query))
-
-	# Construct the Flux query to retrieve only one variable and timestamp
+	# Construct the Flux query for the available time points with multiple variables
 	query = f'''
-	from(bucket: "{bucket}")
-		|> range(start: time(v: "{start_time}"))
-		|> filter(fn: (r) => r["_measurement"] == "{measurement}")
-		|> filter(fn: (r) => r["_field"] == "SEB45Salinity")  
-		|> keep(columns: ["_time", "_value"])  
-	'''
-	# Query the data
-	events = list(query_api.query_stream(org=org, query=query))   
-
-	# Construct the Flux query for multiple variables
-	#query = f'''
-	#		from(bucket: "{bucket}")
-	#			|> range(start: time(v: "{start_time}"))
-	#			|> filter(fn: (r) => r["_measurement"] == "{measurement}")
-	#			|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
-	#		'''
+			from(bucket: "{bucket}")
+				|> range(start: time(v: "{start_time}"))
+				|> filter(fn: (r) => r["_measurement"] == "{measurement}")
+				|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
+			'''
 
 	# Query the data
-	#events = list(query_api.query_stream(org=org, query=query))
+	events = list(query_api.query_stream(org=org, query=query))
 
+	
+	if len(events) > 1: 						# Need at least 3 events to predict next.
+		for i in range(len(events)):			# Iterate over each data point.
+			event = events[i]				# The data point expressed by N-dimensional vector at time point T .
+			timestamp = event["_time"]  		# Extract timestamp.
+				
+			# Iterate over each variable excluding "_time"
+			for variable, value in event.items():
+				if variable == "_time": continue
 
-	if len(events) > 1: 						# Need at least 4 to predict next and compare
+				# Send variable's value (at time T) to the variable LDA's "bach_events" argument stored in the state dictionary.
+				state[variable]["batch_events"].append(value)
+				
+				# Send the variable's next value (at time T+1) to the variable LDA's "next_event" argument stored in the state dictionary. 
+				# This is used for an LDA to predict the next event when  0 <= T < 7. The 7th event is the last one predicted.
+				if i < 7: 
+					state[variable]["next_event"] = events[i+1][variable]	 	
 
-		for i in range(len(events)):
-			batch_events.append(events[i])
-			if i < 7: 
-				next_event = events[i+1]	 	# used when  0 <= T < 7 to predict the next event. The 7th event is the last one predicted.
+				# Run anomaly detection of the specific variable at time T with updated state.
+				anomaly = is_anomaly(T, variable, state)
+				
+				# Print result
+				print(f"Timestamp: {timestamp}, Variable: {variable}, Value: {value}, Anomaly: {anomaly}")
+				
 			
-			# Print the default outputs when T=0 and T=1 																				### delete later
-			if i in [0,1]: 																												### delete later
-				write_result(batch_events[-1].get_time(), i, batch_events[-1].get_value(), predicted_value[-1], 0, 0, write_api)	### delete later
-			else:
-				# RePAD2 Algorithm
-				batch_events, next_event, M, actual_value, predicted_value, sliding_window_AARE, flag = individual_LDA(T, batch_events, next_event, M, flag, actual_value, predicted_value, sliding_window_AARE)
+			
 
-				if flag==False:
-					print(T, batch_events[-1].get_time())
+
+			
+			#batch_events, next_event, M, actual_value, predicted_value, sliding_window_AARE, flag = is_anomaly(T, batch_events, next_event, M, flag, actual_value, predicted_value, sliding_window_AARE)
+
+			#if flag==False:
+			#	print(T, batch_events[-1].get_time())
+
 			# Increment T
 			T += 1
 
