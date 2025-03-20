8718fe7b41b076204ac642a7d9b45b34c3d3da01
DirarSweidan <dirar.sweidan.research@gmail.com>
2025-03-21 00:23:44 +0100

    Update RoLA_model.py

--- models/RoLA_model.py
+++ models/RoLA_model.py
@@ -1,5 +1,52 @@
 ### RoLA Algorithm ###
+from scipy.stats import pearsonr
 
+def get_previous_values(bucket, measurement, timestamp, num_values, org, url, token, , username, password):
+    """
+    This function queries the last "num_values" of a single "measurement" before "timestamp" from 
+	InfluxDB multi-dimensional dataset in order to compute the correlation coeffecient.
+	If velues before the time stamp are less than "num_values" it gets all previous values.
+
+    Parameters:
+	===========
+    - bucket (str): 		InfluxDB bucket name.
+    - measurement (str):	The variable name to extract.
+    - timestamp (str): 		The reference timestamp in RFC3339 format (e.g., "2024-03-20T00:00:00Z").
+    - num_values (int): 	The number of values (p) to extract.
+    - org (str): 			InfluxDB organization name.
+    - url (str): 			InfluxDB server URL.
+    - token (str): 			Authentication token.
+	- username (str):		Authentication user name.
+	- password (str):		Authentication password.
+
+    Returns:
+    - List of extracted values for the given variable.
+    """
+
+	client = InfluxDBClient(url=url, token=token, org=org, , username=username, password=password))
+	query_api = client.query_api()
+
+	# Construct the Flux query to extract one variable's values from a multi-dimensional dataset
+	query = f'''
+    from(bucket: "{bucket}")
+	  |> range(start: -∞)
+	  |> filter(fn: (r) => r["_measurement"] == "{measurement}")
+	  |> filter(fn: (r) => r["_time"] < time(v: "{timestamp}"))  // Before the timestamp
+	  |> sort(columns: ["_time"], desc: true)
+	  |> limit(n: {num_values})  // Extract up to num_values
+		'''
+
+	# Execute query
+	results = query_api.query(query=query, org=org)
+
+	# Extract values
+	values = [record.get_value() for table in results for record in table.records]
+
+	#print(f"Extracted values of '{measurement}' before {timestamp}:")
+	return values
+	
+	
+	
 def is_anomaly(T, variable_name, state):
 	"""
     This function is an LDA-based anomaly detection function that checks if a given
@@ -10,22 +57,24 @@ def is_anomaly(T, variable_name, state):
 	
 	Parameters:
 	===========
-	T:					The given time point of the data point.
-	variable_name:		The name of the variable of the data point. 
-	state:				A nested dictionary containes dictionaries associated with each variable, which containes
-						specific arguments for an LDA to store and update relevant data, such as:
+	- T (int):				The given time point of the data point.
+	- variable_name (str):	The name of the variable of the data point. 
+	- state (dict):			A nested dictionary containes dictionaries associated with each variable, which containes
+							specific arguments for an LDA to store and update relevant data, such as:
 	
-	  *	batch_events:	A batch (type deque) of four time points events D_T-3, D_T-2, D_T-1, and D_T. It should be updated in each iteration.
-						It is used for predicting D_T+1 using batch_events[1:], and predicting D_T using batch_events[0:-1].	
-	  *	next_event:		The event to predict next when T = 0, 1, 2, 3, 4, 5, and 6. It should be updated in each iteration.
-	  *	M:				A trained LSTM model. Default value is "None". 
-	  *	flag:			A flag that indicates whether an anomaly was ditected (falg=False) in the previous iteration. Default value is "True".
-	  *	actual_value:	A deque type window of three elements to store the actual value of events within three iterations to calculate the AARE.
-	  *	predicted_value: A deque type window of three elements to store the predicted value of events within three iterations to calculate the AARE.
-	  *	sliding_window_AARE: A deque type window to store the AARE resulted in each iteration in order to calculate the threhold later.
+	* batch_events (deque): A batch of four time points events D_T-3, D_T-2, D_T-1, and D_T. It should be updated in each iteration.
+							It is used for predicting D_T+1 using batch_events[1:], and predicting D_T using batch_events[0:-1].	
+	* next_event (deque):	The event to predict next when T = 0, 1, 2, 3, 4, 5, and 6. It should be updated in each iteration.
+	* M (object):			A trained LSTM model. Default value is "None". 
+	* flag (bool):			A flag that indicates whether an anomaly was ditected (falg=False) in the previous iteration. Default value is "True".
+	* actual_value (deque):	A deque type window of three elements to store the actual value of events within three iterations to calculate the AARE.
+	* predicted_value (deque):	A deque type window of three elements to store the predicted value of events within three iterations to calculate the AARE.
+	* sliding_window_AARE (deque): A deque type window to store the AARE resulted in each iteration in order to calculate the threhold later.
 	
-	Return:				The flag indicating the anomaly, together with updated batch events, next events, actual_value, predicted_value, 
-	=======				sliding_window_AARE, and the model that will be used in the next iteration.
+	Return:	
+	=======	
+	The flag indicating the anomaly, together with updated batch events, next events, actual_value, predicted_value, 
+	sliding_window_AARE, and the model that will be used in the next iteration.
     """
 	
 	# For printing the values
@@ -191,6 +240,8 @@ query_api = client.query_api()
 T = 0											# Time point
 A, L_var, L_data       = [], [], []					# Lists for storing polling results
 C_agree, C_disagree = 0, 0						# Counters
+p                               = 2880						# The interval between two time points used in computing the correlation between two variables
+thd_pos, thd_neg      = 0.95, -0.95					# The positive and negative Pearson's correlation coefficient thresholds
 poll_interval = 1  								# Time parameter: Second(s)
 time_increment = 1 								# Time parameter: Second(s)
 start_time = "2021-10-28T00:00:00Z" 				# The timestamp of the first event to start with in the given time series dataset
@@ -207,14 +258,15 @@ state        = {
 				 "predicted_value": 	deque([0] * 3, maxlen=3), 
 				 "sliding_window_AARE": deque(maxlen=8064), 
 				 "M": 				None, 
-				 "flag": 			True }
+				 "flag": 			True 
+				 }
 			for key in variables 
 			}
 
    
 	
 while True:
-	# Construct the Flux query for the available time points with multiple variables
+	# Construct the Flux query to extract the available data points from a multi-dimensional dataset
 	query = f'''
 			from(bucket: "{bucket}")
 				|> range(start: time(v: "{start_time}"))
@@ -236,47 +288,52 @@ while True:
 			for variable, value in event.values.items():
 				if variable in ["result", "table","_start","_stop","_time","_measurement","host"]: continue
 
-				# Send variable's value (at time T) to the variable LDA's "bach_events" argument stored in the state dictionary.
+				# Send the value (at time T) to the LDA's "bach_events" argument stored in the state dictionary.
 				state[variable]["batch_events"].append(value)
 				
-				# Send the variable's next value (at time T+1) to the variable LDA's "next_event" argument stored in the state dictionary. 
-				# This is used for an LDA to predict the next event when  0 <= T < 7. The 7th event is the last one predicted.
+				# Send the next value (at time T+1) to the LDA's "next_event" argument stored in the state dictionary. 
+				# This is used for the LDA to predict the next event when  0 <= T < 7. The 7th event is the last one predicted.
 				if i < 7: 
 					state[variable]["next_event"] = events[i+1][variable]	 	
 
-				# Run anomaly detection of the specific variable at time T with updated state.
+				# Run anomaly detection of the current variable at time T with updated state.
 				anomaly = is_anomaly(T, variable, state)
 				if anomaly:
 					A.append(variable)
-			
+
+			# Calculate the Pearson's correlation coefficients between the anomalous variables and proceed with polling
 			if len(A) > 0:
 				for y in range(0,len(A)):
 					C_agree      	= 1
 					C_disagree 	= 0
 					L_var   		= []
 					L_data 		= []
-					a = A[y]			; L_var.append(a)
-					Sa_T = event[a]	; L_data.append(Sa_T)
+					a = A[y]			
+					L_var.append(a)
+					Sa_T = event[a]
+					L_data.append(Sa_T)
+					
+					# Correlations process
 					for z in range(len(variables)):
 						if variables[z] not a:
 							b = variables[z]
-							E_ab = 
-					
-					
-				# Print result
-				#if anomaly:
-					#print(f"T: {T}, Timestamp: {timestamp}, Variable: {variable}, Value: {value}, Anomaly: {anomaly}")
-				
-			
-			
-
-
-			
-			#batch_events, next_event, M, actual_value, predicted_value, sliding_window_AARE, flag = is_anomaly(T, batch_events, next_event, M, flag, actual_value, predicted_value, sliding_window_AARE)
-
-			#if flag==False:
-			#	print(T, batch_events[-1].get_time())
-
+							a_values = get_previous_values(bucket=bucket, measurement=a, timestamp=timestamp, num_values=p, org=org, url=influxdb_url, token=token,username=username, password=password)
+							b_values = get_previous_values(bucket=bucket, measurement=b, timestamp=timestamp, num_values=p, org=org, url=influxdb_url, token=token,username=username, password=password)
+							E_ab, _ =  pearsonr(a_values, b_values)
+							
+							# Polling process
+							if abs(E_ab) >= thd_pos:
+								if b in A:
+									C_agree += 1
+									L_data.append(value)
+									L_var.append(b)
+								else:
+									C_disagree += 1
+					if (C_agree > C_disagree) and ( (C_agree + C_disagree) > 1):
+						for T_data, T_var in zip(L_data, L_var):
+							print(timestamp, T_data, T_var)
+			# Reset A
+			A = []
 			# Increment T
 			T += 1
 
